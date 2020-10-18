aee9f83d8646b33430210ad19c8a0fe806451c20
nclarkinwright <49002688+nclarkinwright@users.noreply.github.com>
2020-10-18 13:23:36 -0400

    Hitting Goal effect added

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    // A static field accessible by code anywhere
+    static public bool goalMet = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // When the trigger is hit by something
+        // Check to see if it's a Projectile
+        if(other.gameObject.tag == "Projectile")
+        {
+            // If so, set goalMet to true
+            Goal.goalMet = true;
+            // Also se the alpha of the color to higher opacity
+            Material mat = GetComponent<Renderer>().material;
+            Color color = mat.color;
+            color.a = 1;
+            mat.color = color;
+        }
+    }
+}
