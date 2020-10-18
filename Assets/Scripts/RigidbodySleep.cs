75c92091ca49ae5f38fab6180bbe188e9d7df133
nclarkinwright <49002688+nclarkinwright@users.noreply.github.com>
2020-10-18 12:30:14 -0400

    Implemented Castle w/ goal

---
+++ Assets/Scripts/RigidbodySleep.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodySleep : MonoBehaviour
+{
+    private void Start()
+    {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        if (rigidbody != null) rigidbody.Sleep();
+    }
+}
