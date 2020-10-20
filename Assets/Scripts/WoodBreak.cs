ae53d6e87e3dcc241f2dbe584bd0cdc2beb31e35
nclarkinwright <49002688+nclarkinwright@users.noreply.github.com>
2020-10-20 12:20:26 -0400

    Added wood material, wood breakage into pieces, 3rd castle

---
+++ Assets/Scripts/WoodBreak.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoodBreak : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public GameObject brokenPiece;
+    public int numBrokenPieces;
+    
+    private Rigidbody rb;
+    private Vector3 vector;
+    private float velocity;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Projectile")
+        {
+            rb = collision.gameObject.GetComponent<Rigidbody>();
+            vector = rb.velocity;
+            velocity = Mathf.Sqrt(Mathf.Pow(vector.x, 2) + Mathf.Pow(vector.y, 2));
+
+            if (velocity >= 10)
+            {
+                Destroy(gameObject);
+                for (int i = 0; i < numBrokenPieces; i++)
+                {
+                    Instantiate<GameObject>(brokenPiece);
+                    brokenPiece.transform.position = gameObject.transform.position;
+                }
+            }
+        }
+    }
+}
